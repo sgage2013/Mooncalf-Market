9c9e0d3805e1d599a2d5ae95ec0540e5c29b30fc
sgage2013 <sgage2013@gmail.com>
2025-06-28 01:58:59 -0500

    added type node to tsconfig.json

--- frontend/src/components/Checkout/Checkout.tsx
+++ frontend/src/components/Checkout/Checkout.tsx
@@ -3,7 +3,6 @@ import { useAppDispatch, useAppSelector } from "../../redux/store";
 import { useNavigate } from "react-router-dom";
 import { ICartState, ICartItem } from "../../redux/types/cart";
 import { getCartItemsThunk } from "../../redux/cart";
-import { checkoutStartThunk } from "../../redux/checkout";
 import CheckoutForm from "./CheckoutForm";
 
 function Checkout() {
