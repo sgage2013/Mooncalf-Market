de38feffdbae3ae98f1f97df5d4e48bd911ed72a
sgage2013 <sgage2013@gmail.com>
2025-06-28 17:16:08 -0500

    replaced dist file

--- backend/dist/utils/date-conversion.js
+++ backend/dist/utils/date-conversion.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateConverter = void 0;
+exports.dateConverter = dateConverter;
 function dateConverter(date) {
     let currDate = new Date(date);
     let dateStr = `${currDate.getFullYear()}-${currDate.getMonth()}-${currDate.getDate()}`;
     return dateStr;
 }
-exports.dateConverter = dateConverter;
 //# sourceMappingURL=date-conversion.js.map
